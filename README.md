f3327c92737e808acbc2f477e6f0731237fb9c1e
Nadeem Chaudhry <chaudhry@arinet.com>
2012-08-13 10:59:22 -0700

    jQuery plugin

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ jquery.mvcparam
 ===============
 
 ASP.NET MVC parameter serialization using jQuery.
+
+Example usage:
+-------------
+
+```js
+var params = {
+                term: 'Footwear',
+                facetValues: [{ Field: 'attr_category', Value: null, Values: ['384'] }],
+                statValues: [],
+                skip: 0,
+                sort: 'relevance',
+                conditions: []
+            };
+var queryString = $.mvcparam(params);
+```
+
+Example output:
+-------------
+
+```c#
+"term=Footwear&facetValues%5B0%5D.Field=attr_category&facetValues%5B0%5D.Value=&facetValues%5B0%5D.Values%5B0%5D=384&skip=&sort=relevance"
+```
+decoded:
+```c#
+"term=Footwear&facetValues[0].Field=attr_category&facetValues[0].Value=&facetValues[0].Values[0]=384&skip=&sort=relevance"
+```
+
+Compared to the original $.param which is not compatible with MVC and outputs:
+```c#
+"term=Footwear&facetValues%5B0%5D%5BField%5D=attr_category&facetValues%5B0%5D%5BValue%5D=null&facetValues%5B0%5D%5BValues%5D%5B%5D=384&skip=0&sort=relevance"
+```
+decoded:
+```c#
+"term=Footwear&facetValues[0][Field]=attr_category&facetValues[0][Value]=null&facetValues[0][Values][]=384&skip=0&sort=relevance"
+```
+
---
+++ jquery.mvcparam.js
@@ -0,0 +1,51 @@
+﻿// MVC param serialization
+(function ($) {
+    // changed: no 'traditional' argument
+    $.mvcparam = function (a) {
+        var s = [],
+		add = function (key, value) {
+		    // If value is a function, invoke it and return its value
+		    value = $.isFunction(value) ? value() : value;
+		    // changed: no value instead of 'null'
+		    s[s.length] = encodeURIComponent(key) + "=" + (value ? encodeURIComponent(value) : '');
+		};
+
+        // If a jQuery element array was passed in, assume that it is an array of form elements.
+        if ($.isArray(a) || (a.jquery && !$.isPlainObject(a))) {
+            // Serialize the form elements
+            $.each(a, function () {
+                add(this.name, this.value);
+            });
+        } else {
+            // Assume this is an array of object
+            for (var prefix in a) {
+                // changed: no need for 'traditional' argument
+                buildParams(prefix, a[prefix], add);
+            }
+        }
+
+        // Return the resulting serialization
+        // changed: inline use of 'r20' variable expression
+        return s.join("&").replace(/%20/g, "+");
+    };
+
+    function buildParams(prefix, obj, add) {
+        if ($.isArray(obj)) {
+            // Serialize array item.
+            $.each(obj, function (i, v) {
+                // changed: always include index bracket
+                buildParams(prefix + "[" + i + "]", v, add);
+            });
+
+        } else if (jQuery.type(obj) === "object") {
+                // Serialize object item.
+            for (var name in obj) {
+                // changed: use dot notation
+                buildParams(prefix + "." + name, obj[name], add);
+            }
+        } else {
+            // Serialize scalar item.
+            add(prefix, obj);
+        }
+    }
+})(jQuery);
